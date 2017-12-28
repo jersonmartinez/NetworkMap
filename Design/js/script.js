944f5b678bcacaec5f6d09d39932837bf3c3ae6e
Side Master <sidemasterdelfuturo@gmail.com>
2017-12-28 08:56:49 -0600

    Ultimas modificaciones

--- Design/js/script.js
+++ Design/js/script.js
@@ -1,4 +1,10 @@
+var myVar = 0;
+
 function StartTracking(){
+	myVar = setInterval(function(){ LoadNetworkMap() }, 2000);
+
+	// setInterval(LoadNetworkMap, 3000);
+
 	$(".btn_tracking span").html("SONDEANDO INFRAESTRUCTURA DE RED...");
 	$.ajax({
 	    url: "php/Tracking.php",
@@ -7,6 +13,27 @@ function StartTracking(){
 			$(".btn_tracking span").html("SONDEAR INFRAESTRUCTURA DE RED");
 			
 			$("#ClickSondeoFinal").click();
+
+			clearInterval(myVar);
+
+			// draw();
+	    	// setTimeout(function(){
+	    	// 	$("#SondeoModal").click();
+	    	// }, 200);
+	    }
+	});
+}
+
+function LoadNetworkMap(){
+	$.ajax({
+	    url: "network/nodeStyles/return.php",
+	    success: function(data){
+	    	$(".here_write").html(data);
+			// $(".btn_tracking span").html("SONDEAR INFRAESTRUCTURA DE RED");
+			
+			$("#ClickSondeoFinal").click();
+
+			// clearInterval(myVar);
 
 			// draw();
 	    	// setTimeout(function(){
